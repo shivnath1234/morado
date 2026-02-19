80e9a4b318a8d5e04ebef772127d6fcd53683746
SHIVNATH DEY <149819054+shivnath1234@users.noreply.github.com>
2026-02-20 03:20:21 +0530

    Add files via upload

---
+++ assets/card-B45OPKTu.js
@@ -0,0 +1,36 @@
+import{c as s,r,j as o,e as d}from"./index-BuvuLLmJ.js";/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z",key:"p7xjir"}]],C=s("cloud",c);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const n=[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]],_=s("database",n);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["rect",{width:"18",height:"11",x:"3",y:"11",rx:"2",ry:"2",key:"1w4ew1"}],["path",{d:"M7 11V7a5 5 0 0 1 10 0v4",key:"fwvmzm"}]],v=s("lock",i);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const l=[["rect",{x:"16",y:"16",width:"6",height:"6",rx:"1",key:"4q2zg0"}],["rect",{x:"2",y:"16",width:"6",height:"6",rx:"1",key:"8cvhb9"}],["rect",{x:"9",y:"2",width:"6",height:"6",rx:"1",key:"1egb70"}],["path",{d:"M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3",key:"1jsf9p"}],["path",{d:"M12 12V8",key:"2874zd"}]],j=s("network",l);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["rect",{width:"20",height:"8",x:"2",y:"2",rx:"2",ry:"2",key:"ngkwjq"}],["rect",{width:"20",height:"8",x:"2",y:"14",rx:"2",ry:"2",key:"iecqi9"}],["line",{x1:"6",x2:"6.01",y1:"6",y2:"6",key:"16zg32"}],["line",{x1:"6",x2:"6.01",y1:"18",y2:"18",key:"nzw8ys"}]],b=s("server",y);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const x=[["path",{d:"M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.106-3.105c.32-.322.863-.22.983.218a6 6 0 0 1-8.259 7.057l-7.91 7.91a1 1 0 0 1-2.999-3l7.91-7.91a6 6 0 0 1 7.057-8.259c.438.12.54.662.219.984z",key:"1ngwbx"}]],z=s("wrench",x);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z",key:"1xq2db"}]],u=s("zap",h),p=r.forwardRef(({className:e,...a},t)=>o.jsx("div",{ref:t,className:d("rounded-lg border bg-card text-card-foreground shadow-sm",e),...a}));p.displayName="Card";const k=r.forwardRef(({className:e,...a},t)=>o.jsx("div",{ref:t,className:d("flex flex-col space-y-1.5 p-6",e),...a}));k.displayName="CardHeader";const m=r.forwardRef(({className:e,...a},t)=>o.jsx("h3",{ref:t,className:d("text-2xl font-semibold leading-none tracking-tight",e),...a}));m.displayName="CardTitle";const w=r.forwardRef(({className:e,...a},t)=>o.jsx("p",{ref:t,className:d("text-sm text-muted-foreground",e),...a}));w.displayName="CardDescription";const f=r.forwardRef(({className:e,...a},t)=>o.jsx("div",{ref:t,className:d("p-6 pt-0",e),...a}));f.displayName="CardContent";const N=r.forwardRef(({className:e,...a},t)=>o.jsx("div",{ref:t,className:d("flex items-center p-6 pt-0",e),...a}));N.displayName="CardFooter";export{p as C,_ as D,v as L,j as N,b as S,z as W,u as Z,k as a,m as b,f as c,w as d,C as e};
