6b23fb2ccd0f91270bbc0349f151c8502f8d9fd0
SHIVNATH DEY <149819054+shivnath1234@users.noreply.github.com>
2026-02-21 04:26:01 +0000

    Add files via upload

---
+++ assets/card-C4H5zEC6.js
@@ -0,0 +1,11 @@
+import{c as o,r as t,j as r,f as d}from"./index-DkB4EE7C.js";/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]],h=o("database",c);/**
+ * @license lucide-react v0.554.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["rect",{x:"16",y:"16",width:"6",height:"6",rx:"1",key:"4q2zg0"}],["rect",{x:"2",y:"16",width:"6",height:"6",rx:"1",key:"8cvhb9"}],["rect",{x:"9",y:"2",width:"6",height:"6",rx:"1",key:"1egb70"}],["path",{d:"M5 16v-3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v3",key:"1jsf9p"}],["path",{d:"M12 12V8",key:"2874zd"}]],N=o("network",i),n=t.forwardRef(({className:e,...a},s)=>r.jsx("div",{ref:s,className:d("rounded-lg border bg-card text-card-foreground shadow-sm",e),...a}));n.displayName="Card";const l=t.forwardRef(({className:e,...a},s)=>r.jsx("div",{ref:s,className:d("flex flex-col space-y-1.5 p-6",e),...a}));l.displayName="CardHeader";const x=t.forwardRef(({className:e,...a},s)=>r.jsx("h3",{ref:s,className:d("text-2xl font-semibold leading-none tracking-tight",e),...a}));x.displayName="CardTitle";const p=t.forwardRef(({className:e,...a},s)=>r.jsx("p",{ref:s,className:d("text-sm text-muted-foreground",e),...a}));p.displayName="CardDescription";const m=t.forwardRef(({className:e,...a},s)=>r.jsx("div",{ref:s,className:d("p-6 pt-0",e),...a}));m.displayName="CardContent";const f=t.forwardRef(({className:e,...a},s)=>r.jsx("div",{ref:s,className:d("flex items-center p-6 pt-0",e),...a}));f.displayName="CardFooter";export{n as C,h as D,N,l as a,x as b,m as c,p as d};
