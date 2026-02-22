f304c97f74175eb67769c311c8f182fb6c6a3cdc
SHIVNATH DEY <149819054+shivnath1234@users.noreply.github.com>
2026-02-22 08:08:26 +0000

    Add files via upload

---
+++ assets/card-D93X8rxf.js
@@ -0,0 +1,13 @@
+import{c as a,r as e,j as s,d as r}from"./index-kV0PEyjJ.js";
+/**
+ * @license lucide-react v0.312.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=a("Cloud",[["path",{d:"M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z",key:"p7xjir"}]]),l=a("Code",[["polyline",{points:"16 18 22 12 16 6",key:"z7tu5w"}],["polyline",{points:"8 6 2 12 8 18",key:"1eg1df"}]]),t=a("Database",[["ellipse",{cx:"12",cy:"5",rx:"9",ry:"3",key:"msslwz"}],["path",{d:"M3 5V19A9 3 0 0 0 21 19V5",key:"1wlel7"}],["path",{d:"M3 12A9 3 0 0 0 21 12",key:"mv7ke4"}]]),o=a("Wrench",[["path",{d:"M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z",key:"cbrjhi"}]]),c=e.forwardRef(({className:a,...e},d)=>s.jsx("div",{ref:d,className:r("rounded-lg border bg-card text-card-foreground shadow-sm",a),...e}));
+/**
+ * @license lucide-react v0.312.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */c.displayName="Card";const i=e.forwardRef(({className:a,...e},d)=>s.jsx("div",{ref:d,className:r("flex flex-col space-y-1.5 p-6",a),...e}));i.displayName="CardHeader";const m=e.forwardRef(({className:a,...e},d)=>s.jsx("h3",{ref:d,className:r("text-2xl font-semibold leading-none tracking-tight",a),...e}));m.displayName="CardTitle";e.forwardRef(({className:a,...e},d)=>s.jsx("p",{ref:d,className:r("text-sm text-muted-foreground",a),...e})).displayName="CardDescription";const f=e.forwardRef(({className:a,...e},d)=>s.jsx("div",{ref:d,className:r("p-6 pt-0",a),...e}));f.displayName="CardContent";e.forwardRef(({className:a,...e},d)=>s.jsx("div",{ref:d,className:r("flex items-center p-6 pt-0",a),...e})).displayName="CardFooter";export{c as C,t as D,o as W,i as a,m as b,f as c,l as d,d as e};
